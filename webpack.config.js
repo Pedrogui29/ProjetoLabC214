05aa6389ba8c62cbf8140c1095f85f8ae0ba31b2
Lucas Couto <lucascouto04@hotmail.com>
2024-11-29 23:28:41 -0300

    Gerando artefato do job build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,14 @@
+const webpack = require('webpack');
 
 module.exports = {
-  
+  // Suas outras configurações aqui
+
+  resolve: {
+    fallback: {
+      crypto: require.resolve('crypto-browserify'),
+    },
+  },
+
   module: {
     rules: [
       {
@@ -12,8 +20,14 @@ module.exports = {
             presets: ["@babel/preset-react", "@babel/preset-env"]
           }
         }
-      },
-      
+      }
     ]
-  }
+  },
+
+  plugins: [
+    // Certifique-se de adicionar este plugin
+    new webpack.ProvidePlugin({
+      process: 'process/browser', // Isso garante que o processo global seja corretamente carregado
+    }),
+  ]
 };
